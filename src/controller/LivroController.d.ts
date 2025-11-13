cd83c38c521d386f8698a1a2844638176b674d7a
joaootavio7 <ojoao21513@gmail.com>
2025-11-13 16:28:45 -0300

    Delete src/controller/LivroController.d.ts

--- src/controller/LivroController.d.ts
+++
@@ -1,8 +0,0 @@
-import type { Request, Response } from "express";
-import Livro from "../model/Livro.js";
-declare class LivroController extends Livro {
-    static todos(req: Request, res: Response): Promise<Response>;
-    static novo(req: Request, res: Response): Promise<Response>;
-}
-export default LivroController;
-//# sourceMappingURL=LivroController.d.ts.map
