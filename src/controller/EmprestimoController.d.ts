38acb512ac0678b224b0df45995a3f4e65d31865
joaootavio7 <ojoao21513@gmail.com>
2025-11-13 16:28:19 -0300

    Delete src/controller/EmprestimoController.d.ts

--- src/controller/EmprestimoController.d.ts
+++
@@ -1,9 +0,0 @@
-import type { Request, Response } from "express";
-import Emprestimo from "../model/Emprestimo.js";
-declare class EmprestimoController extends Emprestimo {
-    static todos(req: Request, res: Response): Promise<Response>;
-    static cadastrar(req: Request, res: Response): Promise<Response>;
-    static atualizar(req: Request, res: Response): Promise<Response>;
-}
-export default EmprestimoController;
-//# sourceMappingURL=EmprestimoController.d.ts.map
