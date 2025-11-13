f1ffd087d3677a678e9ef036e41acd9df351ec04
joaootavio7 <ojoao21513@gmail.com>
2025-11-13 16:32:07 -0300

    Delete src/model/DatabaseModel.d.ts

--- src/model/DatabaseModel.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class DatabaseModel {
-    private _config;
-    private _pool;
-    private _client;
-    constructor();
-    testeConexao(): Promise<boolean>;
-    get pool(): import("pg").Pool;
-}
-//# sourceMappingURL=DatabaseModel.d.ts.map
