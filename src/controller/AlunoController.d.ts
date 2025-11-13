0d44ce2ee75ab4698e3d7e112669ba6af7b389bc
joaootavio7 <ojoao21513@gmail.com>
2025-11-13 16:27:56 -0300

    Delete src/controller/AlunoController.d.ts

--- src/controller/AlunoController.d.ts
+++
@@ -1,9 +0,0 @@
-import Aluno from "../model/Aluno.js";
-import type { Request, Response } from "express";
-declare class AlunoController extends Aluno {
-    static todos(req: Request, res: Response): Promise<Response>;
-    static novo(req: Request, res: Response): Promise<Response>;
-    static aluno(req: Request, res: Response): Promise<Response>;
-}
-export default AlunoController;
-//# sourceMappingURL=AlunoController.d.ts.map
